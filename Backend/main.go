d4120e9f19ce7dc5aaf6e7c70405fabeedd50fcf
Walid Jeghnine <jeghninewalid@outlook.fr>
2024-09-04 12:53:28 +0200

    test scheduler

--- Backend/main.go
+++ Backend/main.go
@@ -11,6 +11,7 @@ import (
 	scheduler "github.com/adatechschool/projet-mobile-pari_damis/Scheduler"
 	"github.com/adatechschool/projet-mobile-pari_damis/database"
 	"github.com/go-co-op/gocron"
+
 	// "github.com/go-co-op/gocron"
 
 	// helper "github.com/adatechschool/projet-mobile-pari_damis/helper"
@@ -29,24 +30,29 @@ import (
 
 func init() {
 	database.ConnectToDatabase()
-	location := time.UTC
-
+	location, err := time.LoadLocation("Europe/Paris")
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	parisTime := time.Now().In(location)
+	fmt.Println("Paris Time ?", parisTime)
 	// Calculer le décalage pour l'Oregon (PST/PDT)
-	utcNow := time.Now().UTC()
+	// utcNow := time.Now().UTC()
 
-	// Heure en Oregon : UTC-8 (en hiver) ou UTC-7 (en été)
-	oregonTime := utcNow.Add(-7 * time.Hour) // Adapte cette ligne pour UTC-7 si c'est l'heure d'été (PDT)
+	// // Heure en Oregon : UTC-8 (en hiver) ou UTC-7 (en été)
+	// oregonTime := utcNow.Add(-7 * time.Hour) // Adapte cette ligne pour UTC-7 si c'est l'heure d'été (PDT)
 
-	// Formater l'heure locale de l'Oregon (ajuster si nécessaire pour l'heure d'été)
-	oregonHour := oregonTime.Hour()
-	oregonMinute := oregonTime.Minute()
-	fmt.Printf("Oregon Time: %02d:%02d (UTC-%d)\n", oregonHour, oregonMinute, 7) // Ajuste à 7 pour l'heure d'été
+	// // Formater l'heure locale de l'Oregon (ajuster si nécessaire pour l'heure d'été)
+	// oregonHour := oregonTime.Hour()
+	// oregonMinute := oregonTime.Minute()
+	// fmt.Printf("Oregon Time: %02d:%02d (UTC-%d)\n", oregonHour, oregonMinute, 7) // Ajuste à 7 pour l'heure d'été
 
 	// Initialiser le scheduler avec UTC
 	myScheduler := gocron.NewScheduler(location)
 
 	// Planifier la tâche à l'heure correspondante (02:46 UTC correspond à 11:46 Paris)
-	myScheduler.Every(1).Wednesday().At("10:45").Do(scheduler.GetMatchAndSaveThemInJson)
+	myScheduler.Every(1).Wednesday().At("12:56").Do(scheduler.GetMatchAndSaveThemInJson)
 
 	// Démarrer le scheduler en mode asynchrone
 	myScheduler.StartAsync()
